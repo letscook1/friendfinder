900b6773f5b1fb4dcd0045eb97d27fa4184a66d0
letscook1 <letscook1@hotmail.com>
2019-06-08 10:00:39 -0400

    okeydokey

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friendsList = [
+  {
+    name: 'Bugs Bunny',
+    picture: '',
+    scores: [4, 3, 4, 2, 4, 5, 3, 3, 5, 5]
+  },
+  {
+    name: 'Marvin the Martian',
+    picture: '',
+    scores: [5, 5, 1, 2, 5, 3, 1, 3, 4, 5]
+  },
+  {
+    name: 'Speedy Gonzales',
+    picture: '',
+    scores: [5, 5, 5, 4, 5, 5, 5, 1, 1, 1]
+  },
+  {
+    name: 'Daffy Duck',
+    picture: '',
+    scores: [5, 2, 5, 1, 4, 1, 3, 1, 5, 5]
+  },
+  {
+    name: 'Yosemite Sam',
+    picture: '',
+    scores: [5, 3, 2, 2, 2, 4, 4, 3, 2, 3]
+  },
+  {
+    name: 'Foghorn Leghorn',
+    picture: '',
+    scores: [4, 4, 3, 2, 3, 4, 4, 1, 4, 5]
+  }
+];
+
+module.exports = friendsList;
